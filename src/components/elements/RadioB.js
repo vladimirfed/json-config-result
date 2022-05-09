bcf963d3847aa5cf367f27968dc181517351d9dc
vladimirfed <61358490+vladimirfed@users.noreply.github.com>
2022-05-09 13:00:47 +0200

    Update RadioB.js

--- src/components/elements/RadioB.js
+++ src/components/elements/RadioB.js
@@ -1,18 +1,30 @@
 import React, { useContext } from "react";
 import { FormContext } from "../../FormContext";
 
-const RadioB = ({ field_id, field_label, field_value }) => {
+const RadioB = ({ input_label, input_value, input_options }) => {
   const { handleChange } = useContext(FormContext);
 
   return (
-    <div className="mb-3 form-check">
-      <input
-        type="radio"
-        className="form-check-input"
-        checked={field_value}
-        onChange={(event) => handleChange(field_id, event)}
-      />
-      <label className="form-check-label">{field_label}</label>
+    <div>
+      <label className="form-check-label">{input_label}</label>
+      {input_options.length > 0 &&
+        input_options.map((option, i) => {
+          return (
+            <div className="form-check" key={i}>
+              <input
+                value={option.option_label}
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+                onChange={(event) => handleChange(input_label, event)}
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                {option.option_label}
+              </label>
+            </div>
+          );
+        })}
     </div>
   );
 };
