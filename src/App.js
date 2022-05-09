fe6b2ac64265ab440c9c8adb4829d87042385d0b
vladimirfed <61358490+vladimirfed@users.noreply.github.com>
2022-05-09 13:01:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,30 @@ import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [items, setItems] = useState(null);
-  const [res, setRes] = useState(null);
 
   const addTodo = (todoInput) => {
-    let res = eval("(" + todoInput + ")"); //  need to refactor
+    let res = JSON.parse(todoInput);
     setItems(res);
   };
 
-  const { fields, page_title } = items ?? {};
+  const { inputs, page_title } = items ?? {};
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(items);
-    setRes(items.fields);
-    // console.log(res)
   };
 
-  const handleChange = (id, e) => {
+  const handleChange = (label, e) => {
     const newitems = { ...items };
-    newitems.fields.forEach((field) => {
-      const { field_type, field_id } = field;
-      if (id === field_id) {
-        switch (field_type) {
-          case "checkbox" || "radio":
-            field["field_value"] = e.target.checked;
+    newitems.inputs.forEach((input) => {
+      const { input_type, input_label } = input;
+      if (label === input_label) {
+        switch (input_type) {
+          case "checkbox":
+            input["input_value"] = e.target.checked;
             break;
           default:
-            field["field_value"] = e.target.value;
+            input["input_value"] = e.target.value;
             break;
         }
       }
@@ -56,32 +53,15 @@ function App() {
           path="/result"
           element={
             <Result
-              fields={fields}
+              inputs={inputs}
               page_title={page_title}
               handleSubmit={handleSubmit}
             />
           }
         />
       </Routes>
-      <div className="App container">
-        {res !== null
-          ? res.map((e) => <div key={e.field_id}>{e.field_value}</div>)
-          : null}
-      </div>
     </FormContext.Provider>
   );
 }
 
 export default App;
-
-// отобразить результат ввода  - логика готова
-
-// валидировать текстарию
-
-// избавиться от евал
-
-// сделать UI - +-поправил накинув бутстрап
-
-// реструктурировать файлы  - добавил нужные
-
-// видоизменить
